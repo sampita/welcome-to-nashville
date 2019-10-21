684449ec632203210164ddb2b523d19ff5eb7cc6
Christian Pippin <ianpippin@gmail.com>
2019-10-21 14:59:07 -0500

    merge conflict check

---
+++ src/scripts/api-manager.js
@@ -0,0 +1,5 @@
+console.log("orange skeletons rock")
+
+const foodTypeSearch(){
+    return "what do you call a Japanese drummer boy, whose father has diarrhea? A slap happy jappy, with a crap happy pappy..."
+}
