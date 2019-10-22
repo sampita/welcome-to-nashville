6591522c1f7836291dc8cb54e9cdfa1d7c9e6014
Sam Pita <samanthakpita@gmail.com>
2019-10-22 09:32:55 -0500

    search form template + search event listeners function

--- src/scripts/event-manager.js
+++ src/scripts/event-manager.js
@@ -1,5 +1,10 @@
 // Purpose: This file handles event listeners.
 
+// TODO: delete fake function after everyone creates their own function 
+const myfunction = () => {
+    console.log("pie")
+}
+
 const attachSearchEvent = () => {
     const concertSearchButton = document.querySelector("#concertsSearchButton")
     concertSearchButton.addEventListener("click", myfunction)
@@ -8,7 +13,7 @@ const attachSearchEvent = () => {
     parksSearchButton.addEventListener("click", myfunction)
 
     const restaurantsSearchButton = document.querySelector("#restaurantsSearchButton")
-    restaurantSearchButton.addEventListener("click", myfunction)
+    restaurantsSearchButton.addEventListener("click", myfunction)
 
     const meetupsSearchButton = document.querySelector("#meetupsSearchButton")
     meetupsSearchButton.addEventListener("click", myfunction)
