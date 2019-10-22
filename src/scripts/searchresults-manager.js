39c4cf329fa6586fc9b190860d90e9517beacafb
Christian Pippin <ianpippin@gmail.com>
2019-10-22 13:15:14 -0500

    created function to append search results to DOM

--- src/scripts/searchresults-manager.js
+++ src/scripts/searchresults-manager.js
@@ -1,6 +1,18 @@
 // This function will append search results to DOM
-const resultsHeader = () => {
-    const searchresults = `
-    <h3 class="" >Results</h3>`
+const resultsFunction = () => {
+    const searchResults = `
+    <div class="card" style="width: 18rem;">
+    <div class="card-header">
+        Results
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Cras justo odio</li>
+      <li class="list-group-item">Dapibus ac facilisis in</li>
+      <li class="list-group-item">Vestibulum at eros</li>
+    </ul>
+  </div>`
+
+  const resultsContainer = document.querySelector(".resultsContainer")
+  resultsContainer.innerHtml = searchResults
 
 }
