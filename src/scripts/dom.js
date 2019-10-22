5030d2a905ab410084d6276f2e77a90499a70be9
Ryan Bishop <rdbishop06@gmail.com>
2019-10-22 11:32:20 -0500

    Render Welcome header to DOM

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,3 +1,13 @@
 const renderPageTitle = () => {
     // TODO: write factory function to create main page header and then call this function in main.js
+    // TODO: get container element
+    let container = document.querySelector("#welcome-title")
+    console.log(container)
+    // TODO: create HTML element
+    let headerEl = document.createElement("h1");
+    headerEl.id = "title";
+    headerEl.innerText = "Welcome to Nashville!"
+    console.log(headerEl);
+    container.appendChild(headerEl)
+    // TODO: write HTML to dom
 }
