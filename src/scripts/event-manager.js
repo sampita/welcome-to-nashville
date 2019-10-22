aaedca00e09c8a22f61077ee76cd4aea122daadf
Sam Pita <samanthakpita@gmail.com>
2019-10-22 09:31:57 -0500

    created and appended Search HTML to DOM and created Event Listener for search buttons

--- src/scripts/event-manager.js
+++ src/scripts/event-manager.js
@@ -1,2 +1,19 @@
 // Purpose: This file handles event listeners.
 
+const attachSearchEvent = () => {
+    const concertSearchButton = document.querySelector("#concertsSearchButton")
+    concertSearchButton.addEventListener("click", myfunction)
+
+    const parksSearchButton = document.querySelector("#parksSearchButton")
+    parksSearchButton.addEventListener("click", myfunction)
+
+    const restaurantsSearchButton = document.querySelector("#restaurantsSearchButton")
+    restaurantSearchButton.addEventListener("click", myfunction)
+
+    const meetupsSearchButton = document.querySelector("#meetupsSearchButton")
+    meetupsSearchButton.addEventListener("click", myfunction)
+
+    
+
+
+}
--- src/scripts/searchform-manager.js
+++ src/scripts/searchform-manager.js
@@ -5,16 +5,16 @@
 const buildSearchForm = () => {
     const searchForm = `
     <input type="text" placeholder="parks by feature">
-    <button>Search</button>
-    <!-- restaurants search  -->
+    <button id="parksSearchButton">Search</button>
+    
     <input type="text" placeholder="restaurants by food type">
-    <button>Search</button>
-    <!-- meetups search -->
+    <button id="restaurantsSearchButton">Search</button>
+    
     <input type="text" placeholder="meetups by topic">
-    <button>Search</button>
-    <!-- concerts search  -->
+    <button id="meetupsSearchButton">Search</button>
+    
     <input type="text" placeholder="concerts by genre">
-    <button>Search</button>
+    <button id="concertsSearchButton">Search</button>
     `
     // Gets reference to Search container in the index.html file and prints the above HTML code to the DOM.
     const searchContainer = document.querySelector(".searchContainer")
