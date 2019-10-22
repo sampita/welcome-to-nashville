c429d372a7c534c8168e3c0c68643d6015bf6f34
Sam Pita <samanthakpita@gmail.com>
2019-10-22 13:17:14 -0500

    working on fetch call for ticketmaster api

--- src/scripts/api-manager.js
+++ src/scripts/api-manager.js
@@ -1,17 +1,20 @@
-console.log("orange skeletons rock")
+// Purpose: The point of this file is to handle all API requests.
+
+ticketmasterApiBaseUrl = "https://app.ticketmaster.com/discovery/v2/"
+
+// TODO: write fetch call for ticketmaster API
+const concertSearch = (ticketmasterApiBaseUrl, ticketmasterApiKey, searchInput) => {
+    const searchField = document.querySelector(".concertsInput")
+    fetch(`${ticketmasterApiBaseUrl}events.json?classificationName=music&dmaId=343&apikey=${ticketmasterApiKey}&q=${searchInput}`)
+    .then(response => response.json())
+    .then(response => console.log(response))
+}  
 
 const foodTypeSearch = function() {
     return "what do you call a Japanese drummer boy, whose father has diarrhea? A slap happy jappy, with a crap happy pappy..."
 }
 
-const concertSearch = event => {
-    const inputField = document.QuerySelector(".searchContainer")
-}  
 
 const getEventbriteData = () => {
     console.log("Here is where the cool stuff will go");
-}
-
-ticketmasterApiBaseUrl = {
-    
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,3 +4,5 @@
 buildSearchForm();
 
 attachSearchEvent();
+
+concertSearch();
